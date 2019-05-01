fc86bd4817e760ee70764b0de66040f38bae12b9
Brian Hilst <brian.hilst@purposefulsoftware.com>
2019-05-01 12:13:40 -0700

    A start

---
+++ src/resumeData.ts
@@ -0,0 +1,194 @@
+const resumeData = {
+  "main": {
+    "title": "Purposeful Software",
+    "name":"Brian Hilst",
+    "occupation":"Software Developer & Architect",
+    "description":"With a depth of experience ranging from large-scale enterprise architecture to responsive web and mobile applications, I am passionate about creating great user experiences through beautifully designed and constructed software. I choose to work as a consultant for the wide variety of projects, ideas and people I get to work with. Having views like this from where I live on Bainbridge Island might be a factor too...",
+    "image":"brian-hilst.jpg",
+    "bio":"I am a versatile, full-stack software engineer and architect.  My stack-of-choice is React JS/Native or iOS with a serverless backend running on Amazon Web Services.  Please see my full skill set below for more information.",
+    "contactmessage":"Contact me if you are looking for beautifully designed and constructed software at a fair price.",
+    "email": "brian@purposefulsoftware.com",
+    "phone": "206-855-7927",
+    "address":{
+    "street":"8150 Hansen Road NE",
+      "city":"Seattle",
+      "state":"WA",
+      "zip": "98110"
+  },
+  "website": "http://www.purposefulsoftware.com",
+    "resumedownload":"http://www.purposefulsoftware.com/resume",
+    "social":[
+    {
+      "name":"facebook",
+      "url":"http://facebook.com/tim.baker.906",
+      "className":"fa fa-facebook"
+    },
+    {
+      "name":"linkedin",
+      "url":"https://www.linkedin.com/in/brianhilst/",
+      "className":"fa fa-linkedin"
+    },
+    {
+      "name":"github",
+      "url":"http://github.com/tbakerx",
+      "className":"fa fa-github"
+    },
+    {
+      "name":"skype",
+      "url":"http://skype.com",
+      "className":"fa fa-skype"
+    }
+  ]
+},
+  "resume":{
+    "education":[
+    {
+      "school":"University of Kansas",
+      "degree":"Bachelor of Science in Computer Science",
+      "graduated":"",
+      "description":"A broad computer science foundation with specialization in algorithms, artificial intelligence, and computer graphics."
+    },
+    {
+      "school":"Continuing Education",
+      "degree":"Conferences, online courses, and books",
+      "graduated":"",
+      "description":"I keep current with software technologies and development practices. My latest reads are Clean Architecture by Robert Martin and Start Small, Stay Small by Rob Walling."
+    }
+  ],
+    "work":[
+    {
+      "company":"SnapGig Inc - Spokane, WA",
+      "title":"Software Architect and Senior Developer",
+      "years":"2018 - Present",
+      "description":"Architect for a gig-economy startup. Designed and developed on a stack using React JS/Typescript, AWS Amplify, AppSync, DynamoDB, Cognito, S3, SES, SNS. A React Native app is in the works."
+    },
+    {
+      "company":"Sears Home Services - Seattle, WA",
+      "title":"Software Architect and Senior Developer",
+      "years":"2015 - 2018",
+      "description":"Architected and built a native iOS app providing routing, navigation, service order management, service manuals, and other functionality for their 7000+ field technicians. Developed a web application for data management and analysis of the mobile app utilizing Angular, SpringData, Java."
+    },
+    {
+      "company":"SamePage Health - Seattle, WA",
+      "title":"Software Architect and Senior Developer",
+      "years":"2014 - 2015",
+      "description":"Director of development for a SaaS-based collaborative health care application providing tools to health care organization for managing patient outcomes and behavioral change. This solution utilizes Ruby on Rails, AngularJS, Postgres, and AWS to deliver responsive web-based tools.  Built REST APIs for AngularJS and mobile clients."
+    },
+    {
+      "company":"Netmotion Wireless - Seattle, WA",
+      "title":"Senior Consultant",
+      "years":"2013 - 2014",
+      "description":"Designed and implemented a mapping web application to display mobile device connectivity and tracking data used to troubleshoot mobile workforces such as police and fire departments.  The solution utilized Bing Maps, ASP.NET, REST, C#, and MySql technologies.  Product was delivered and released on schedule."
+    },
+    {
+      "company":"Avalara Inc - Seattle, WA",
+      "title":"Chief Architect / Principal Software Developer",
+      "years":"2004 - 2013",
+      "description":"Founding team member, software architect and principal developer of Avalara’s industry-leading, SaaS, transactional tax service handling over a million transactions per day. Key technologies included SQL Server, Microsoft.NET, C#, Java."
+    },
+    {
+      "company":"Rainwave Technologies - Bellevue, WA",
+      "title":"Owner / Principal Software Developer",
+      "years":"1996 - 2004",
+      "description":"Operated my own consulting agency working on web and desktop clients for companies such as Autonomy Interwoven & FrontOffice Technologies"
+    },
+    {
+      "company":"Adobe Systems / Aldus Corp - Seattle, WA",
+      "title":"Principal Software Developer",
+      "years":"1990 - 1995",
+      "description":"Worked as a Core Technologies Team Lead and Pagemaker Graphics Team Lead contributing to products such as PageMaker and other desktop graphics applications"
+    },
+  ],
+    "skillmessage":"Here is a summary of my top skills.",
+    "skills":[
+    {
+      "name":"Software Design & Planning",
+      "description": "I practice iterative software design and implementation focused on getting an MVP (Minimum Viable Product) to market quickly to test the design and market in the real world.  I design for scale from the ground-up.  I recommend collaborative design tools such as LucidChart and InVision and lightweight project management tools such as Trello and Jira.",
+      "level":"60%"
+    },
+    {
+      "name":"Web Apps",
+      "description": "An effective web application is necessary to promote and convert visitors to customers. Mobile responsiveness is critical for most applications. My preferred frameworks are React JS or Angular for their broad community of support.",
+      "level":"50%"
+    },
+    {
+      "name":"Mobile Apps",
+      "description": "A first-rate mobile app is a key component of most projects. Depending on the requirements, this can range from native iOS and Android to cross-platform React Native or an Ionic hybrid.",
+      "level":"90%"
+    },
+    {
+      "name":"Cloud Services",
+      "description": "Typically this involves a combination Amazon Web Services with legacy integration or migration as needed. For new projects, I recommend serverless using on-demand AWS services including Amplify, AppSync, DynamoDB and Lambda for rapid development and scalability.",
+      "level":"80%"
+    },
+    {
+      "name":"Domain Expertise",
+      "description": "I have  broad range of domain expertise ranging from field workforce automation, mapping, e-commerce, transaction taxes, analytics and data-visualization.",
+      "level":"50%"
+    },
+    {
+      "name":"Languages",
+      "description": "My current favorite language is TypeScript, but I also have a lot of experience in Java, Swift, Ruby, Javascript and C#. It seems like every project involved at least one new language. I'm also learning Spanish!",
+      "level":"80%"
+    },
+    {
+      "name":"Tools & Services",
+      "description": "My favorite tools include JetBrains WebStorm, IntelliJ, RubyMine, XCode, & Photoshop. Testing tools like Jest, Cypress, BrowserStack, and Postman are critical.  I also depend on collaborative services like GitHub, LucidChart, Jira, Trello, & Slack.",
+      "level":"55%"
+    },
+    {
+      "name":"Team Building and Leadership",
+      "description": "My team experience and skill-set range from independent projects to leading internationally-distributed teams. I've helped hire and build startup teams and managed onsite and remote teams in India and Central America.",
+      "level":"55%"
+    },
+  ]
+},
+  "portfolio":{
+  "projects": [
+    {
+      "title":"SnapGig.com",
+      "category":"On demand mobile workforce",
+      "image":"snapgig-2.png",
+      "url":"https://www.snapgig.com"
+    },
+    {
+      "title":"SnapGig.com",
+      "category":"On demand mobile workforce",
+      "image":"snapgig-3.png",
+      "url":"https://www.snapgig.com"
+    },
+    {
+      "title":"TechHub",
+      "category":"Native iOS App for Sears Technicians",
+      "image":"techhub-1.png",
+      "url":"http://www.purposefulsoftware.com"
+    },
+    {
+      "title":"SamePage Health",
+      "category":"Health care web app",
+      "image":"samepage-1.png",
+      "url":"https://www.samepagehealth.com"
+    },
+    {
+      "title":"Netmotion Wireless",
+      "category":"Mobile connectivity tracking web site",
+      "image":"netmotion-1.png",
+      "url":"http://www.netmotionsoftware.com"
+    },
+  ]
+},
+  "testimonials":{
+  "testimonials":[
+    {
+      "text":"Here you should add some nice things that someone has said about you.",
+      "user":"Joe Client"
+    },
+    {
+      "text":"That Brian Hilst must be one of the most brilliant developers I've ever met! It is amazing that nobody has hired him yet. Hey you, you should hire this guy, he may be fresh out of University and have zero on the job experience but I am confident that he will be one of your best developers in no time!",
+      "user":"Steve Wozniak... impersonator"
+    }
+  ]
+}
+}
+
+export default resumeData
